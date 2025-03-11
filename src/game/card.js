7292cda352a243954299361accd47d946a768fa9
leekery <pashokmoreplay@gmail.com>
2025-03-12 01:08:54 +0300

    rebuild stucture

--- src/game/card.js
+++
@@ -1,9 +0,0 @@
-export default class Card {
-  constructor({ id, name, cost, type, description }) {
-    this.id = id
-    this.name = name
-    this.cost = cost
-    this.type = type // "attack", "skill", ...
-    this.description = description
-  }
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Game from './game/game.js'
-
-const game = new Game()
-game.init()
-
-console.log("Игра запускается")
