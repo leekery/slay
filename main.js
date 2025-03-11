a58c933e4633f5667744fcceb221676cab158a8f
leekery <pashokmoreplay@gmail.com>
2025-03-12 00:27:19 +0300

    Начальная структура для Slay the Spire

---
+++ main.js
@@ -0,0 +1,25 @@
+// main.js
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+  win.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
